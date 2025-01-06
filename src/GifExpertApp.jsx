a1d57b159a43b22a5305744567cec78407a4d0da
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-06 19:25:54 -0400

    [Section 6]: Add New Component Gif Grid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
@@ -21,24 +22,16 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/* titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* Input Output */}
       <AddCategory
-        // setCategories={setCategories} \
         onNewCategory={(value) => onAddCategory(value)}
         currentCategories={categories}
       />
-      {/* Listado de Gif*/}
-      {/* <button onClick={onAddCategory}>Agregar</button> */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-        {/* <li>ABC</li> */}
-      </ol>
-      {/*Gif Item*/}
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
