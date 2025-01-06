299faee2e9523bcf6b54e44c19d591d5f69cd755
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-06 19:08:16 -0400

    [Section 6]: Validate that values are unique

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,35 +1,44 @@
-import {useState} from 'react'
-import { AddCategory } from './components/AddCategory'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-   const [categories, setCategories] = useState([ 'One Punch','Dragon Ball' ]) 
+  const onAddCategory = (newCategory) => {
+    // if (categories.includes(newCategory)) return;
 
-   const onAddCategory= ()=>{ 
-        // setCategories([...categories, 'One Piece'])
-        setCategories(cat => [...cat, 'Onepiece'])
-       
-        
-     }
-    
+    const categoryExists = categories.find((category) => {
+      return category.toLowerCase() === newCategory.toLowerCase();
+    });
+
+    if (categoryExists) return;
+    // console.log(newCategory);
+
+    // categories.push(newCategory)
+    // setCategories([...categories, 'One Piece'])
+    setCategories((cat) => [newCategory, ...cat]);
+  };
+
   return (
     <>
-        {/* titulo */}
-        <h1>GifExpertApp</h1>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
 
-        {/* Input Output */}
-        <AddCategory setCategories={setCategories} />
-        {/* Listado de Gif*/}
-        {/* <button onClick={onAddCategory}>Agregar</button> */}
-        <ol>
-            {
-            categories.map(category => {
-                return <li key={category}>{category}</li>
-            })
-            }
-            {/* <li>ABC</li> */}
-        </ol>
-        {/*Gif Item*/}
+      {/* Input Output */}
+      <AddCategory
+        // setCategories={setCategories} \
+        onNewCategory={(value) => onAddCategory(value)}
+        currentCategories={categories}
+      />
+      {/* Listado de Gif*/}
+      {/* <button onClick={onAddCategory}>Agregar</button> */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+        {/* <li>ABC</li> */}
+      </ol>
+      {/*Gif Item*/}
     </>
-  )
-}
+  );
+};
