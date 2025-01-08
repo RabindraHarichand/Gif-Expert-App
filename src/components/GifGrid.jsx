7946bc3b2db2bdd6fde741dca22f67c83cf3b141
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-08 10:43:08 -0400

    [Section 6]: Add loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,11 +4,12 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 export const GifGrid = ({ category }) => {
   const { images, isLoading } = useFetchGifs(category);
 
-  console.log({ isLoading });
-
   return (
     <>
       <h3>{category}</h3>
+      {/* {isLoading ? <h2>Cargando...</h2> : null} */}
+      {/* <LoadingMessage isLoading={isLoading}/> */}
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid">
         {
           /* {images.map()} */
